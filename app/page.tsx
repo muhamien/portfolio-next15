8b1df27dade8e426562bf130c1e5fa47381e424f
Muhammad Amien <muhammadamien23@gmail.com>
2025-10-19 19:58:59 +0700

    Remove Experience component from Home page rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 'use client'
 
 import Hero from "@/components/home/Hero";
-import Experience from "@/components/home/Experience";
+// import Experience from "@/components/home/Experience";
 import ContributionCalendar from "@/components/home/ContributionCalendar";
 
 export default function Home() {
   return (
     <div className="home-container">
       <Hero />
-      <Experience />
+      {/* <Experience /> */}
       <ContributionCalendar />
     </div>
   );
